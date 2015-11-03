9187e128cece237e1af438fe18b140a103b25d7f
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2015-11-03 11:29:40 -0500

    Create basic showfile command

--- showfile.c
+++ showfile.c
@@ -1,53 +1,97 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "flop.h"
 #include "fmountutils.h"
 #include "structure.h"
 #include "fatroot.h"
 #include "showsector.h"
+#include "concatbytes.h"
 
+static int print_file_dump(struct FlopData *, struct rootent *);
+
+// Shows a hex dump of the file given in argv
 int command_showfile(struct FlopData *flopdata, int argc, char **argv) {
 
 	static const int ROOT_ENTRY_SIZE = 32;
 
-	if(argc != 2) {
+	if (argc != 2) {
 		fprintf(stderr, "Invalid number of arguments.\nUsage: showfile [filename]\n");
 		return 1;
 	}
 
-	if(!has_mounted_image(flopdata)) {
+	if (!has_mounted_image(flopdata)) {
 		fprintf(stderr, "Error. No image mounted.\n");
 		return 1;
 	}
-	
-	if(load_fs_structure(flopdata) != 0) {
+
+	if (load_fs_structure(flopdata) != 0) {
 		fprintf(stderr, "Error. Filesystem structure data could not be loaded.\n");
 		return 1;
 	}
-	
-	
-	
+
+	int foundFile = 0;
 	char *filename = argv[1];
+	char *rootDir = flopdata->rawData + calc_root_start_sector(flopdata) * flopdata->bytesPerSector;
 	int i;
-	for(i = 0; i < flopdata->nRootEntries; i++) {
-		char *curEntData = flopdata->rawData + calc_root_start_sector(flopdata)*flopdata->bytesPerSector + i*ROOT_ENTRY_SIZE;
+	for (i = 0; i < flopdata->nRootEntries; i++) {
+		char *curEntData = rootDir + i * ROOT_ENTRY_SIZE;
 		struct rootent *curEnt = malloc(sizeof(*curEnt));
 		parse_rootent(curEntData, curEnt);
-		
+
 		char filename_full[13];
 		filename_full[0] = '\0';
 		strcpy(filename_full, curEnt->filename);
 		strcat(filename_full, ".");
 		strcat(filename_full, curEnt->fileext);
-		
-		if(strcasecmp(filename_full, filename) == 0) {
-			printf("%s\n", filename_full);
+
+		if (strcasecmp(filename_full, filename) == 0) {
+			print_file_dump(flopdata, curEnt);
+			foundFile = 1;
+			break;
 		}
-		
+
 		free(curEnt);
 	}
-	
+
+	if (!foundFile) {
+		fprintf(stderr, "File not found.\n");
+	}
+
+	return 0;
+}
+
+// Prints a file starting at the given cluster as a hex dump
+static int print_file_dump(struct FlopData *flopdata, struct rootent *fileEnt) {
+	int *clusters = malloc(5 * sizeof(int));
+	int nClusters = 0;
+	char *fatData = flopdata->rawData + (flopdata->nReservedSectors * flopdata->bytesPerSector);
+
+	int data_clusters_offset =
+	    calc_root_start_sector(flopdata) + ((32 * flopdata->nRootEntries) / flopdata->bytesPerSector) - 2;
+
+	unsigned int nextCluster = fileEnt->first_cluster;
+	while (nextCluster < 0xFF8) {
+		if (nClusters % 5 == 0 && nClusters != 0) {
+			clusters = realloc(clusters, (nClusters + 5) * sizeof(int));
+		}
+		clusters[nClusters++] = data_clusters_offset + nextCluster;
+
+		char *entry = fatData + ((nextCluster * 12) / 8);
+		printf("%d : %lu\n", nextCluster, data_clusters_offset);
+		if (nextCluster % 2 == 0) {
+			nextCluster = concat_uint8_uint16(entry[1] & 0x0F, entry[0]);
+		} else {
+			nextCluster = concat_uint8_uint16(entry[1], entry[0] & 0xF0);
+		}
+		sleep(1);
+	}
+
+	show_sectors(flopdata, clusters, nClusters);
+
+	free(clusters);
+
 	return 0;
 }
--- showsector.c
+++ showsector.c
@@ -9,14 +9,14 @@
 #include <stdio.h>
 
 
-// Prints a hex dump of the sector 
+// Prints a hex dump of the sector
 int command_showsector(struct FlopData *flopdata, int argc, char **argv) {
-	
+
 	if(argc != 2) {
 		fprintf(stderr, "Error. Invalid number of arguments. Please specify a sector number.\n");
 		return 1;
 	}
-	
+
 	int sec = atoi(argv[1]);
 	return show_sectors(flopdata, &sec, 1);
 }
@@ -28,22 +28,22 @@ int show_sectors(struct FlopData *flopdata, int *sectornums, int nSectors) {
 		fprintf(stderr, "Error. No image mounted.\n");
 		return 1;
 	}
-	
+
 	if(load_fs_structure(flopdata) != 0) {
 		fprintf(stderr, "Error. Filesystem structure data could not be loaded.\n");
 		return 1;
 	}
-	
+
 	if(0 < nSectors) {
-		print_hex_dump(flopdata->rawData + flopdata->bytesPerSector * sectornums[0], flopdata->bytesPerSector);
+		print_hex_dump(flopdata->rawData + (flopdata->bytesPerSector * sectornums[0]), flopdata->bytesPerSector);
 	}
 
 	int i;
 	for(i = 1; i < nSectors; ++i) {
 		off_t offset = flopdata->bytesPerSector * sectornums[i];
 		append_hex_dump((flopdata->rawData + offset), flopdata->bytesPerSector, i*flopdata->bytesPerSector);
-		
 	}
 	fprintf(stdout, "\n");
+
 	return 0;
 }
