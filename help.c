6516de8dfa34b32bab21fa95eaf66ecff198df67
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2015-11-08 17:29:28 -0500

    Reset README and help.c to prepare for proj4 spcifications

--- help.c
+++ help.c
@@ -12,16 +12,5 @@ int command_help(struct FlopData *flopdata, int argc, char **argv) {
 // Print help
 void print_help() {
 
-        printf("\nThe shell has the following commands.\n\n");
-        printf("usage___________________________|result_______________________\n");
-        printf("fmount [path to image]\t\t|mounts the image to the shell.\n");
-        printf("fumount \t\t\t|tunmounts image from shell.\n");
-        printf("showfat\t\t\t\t|shows the fat1 table for mounted image.\n");
-        printf("traverse [-l]\t\t\t|shows directory entries on image.\n");
-        printf("showfile [directory entry]\t|shows file raw data.\n");
-	printf("structure\t\t\t|shows structure of disk.\n");
-	printf("showsector [sector #]\t\t|show specified sector #.\n");
-        printf("quit or q\t\t\t|exit out of the shell.\n\n");
-
 }
 
