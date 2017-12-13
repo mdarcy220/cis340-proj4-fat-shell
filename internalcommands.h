96bbd377ea7a6466d67eb94b5749e312ef65248b
Mike D'Arcy <m.darcy.220@gmail.com>
2017-12-13 02:43:15 -0500

    License under GNU GPL

--- internalcommands.h
+++ internalcommands.h
@@ -1,3 +1,23 @@
+
+/*
+ * Copyright (C) 2017 Mike D'Arcy
+ *
+ * This file is part of FlopShell, a toy shell utility for floppy disk images.
+ * 
+ * FlopShell is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * FlopShell is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with FlopShell.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef INTERNAL_COMMANDS_H_
 #define INTERNAL_COMMANDS_H_
 
